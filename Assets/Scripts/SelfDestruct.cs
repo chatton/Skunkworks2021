d02713b0597da10132d307eb399ca15ce93ba319
Rajdeep Das <rajdeep.das@mongodb.com>
2021-12-02 17:38:03 +0100

    Bootstrapping Unity

--- Assets/Scripts/SelfDestruct.cs
+++ Assets/Scripts/SelfDestruct.cs
@@ -1,9 +1,13 @@
 
 using UnityEngine;
 
+
 public class SelfDestruct : MonoBehaviour
 {
-    
+
+    private void Start() {
+        Debug.Log("yolo");
+    }    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))

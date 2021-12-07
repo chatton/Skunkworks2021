40cd6df2e0c73ebb2f0997acab40338e056af42f
Cian Hatton <cianhatton@gmail.com>
2021-12-07 14:51:49 +0000

    Added kill trigger for player

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -11,6 +11,7 @@ public class Monster : MonoBehaviour
     private Animator _animator;
     private BoxCollider _collider;
     private Rigidbody _rigidbody;
+    private AreaTrigger _areaTrigger;
 
     private static readonly int Die = Animator.StringToHash("Die");
     private static readonly int Attacking = Animator.StringToHash("Attacking");
@@ -19,21 +20,23 @@ public class Monster : MonoBehaviour
     private void Awake()
     {
         _animator = GetComponentInChildren<Animator>();
+        _areaTrigger = GetComponentInChildren<AreaTrigger>();
         _collider = GetComponent<BoxCollider>();
         _rigidbody = GetComponent<Rigidbody>();
+        ;
     }
 
     private void Start()
     {
         // get a reference to the player's health, any time the player runs into an obstacle they will take damage.
         _playerHealth = FindObjectOfType<Player>().GetComponent<Health>();
+        _areaTrigger.Callback += PlayerEnteredDeathBox;
     }
 
 
-    private void OnCollisionEnter(Collision other)
+    private void PlayerEnteredDeathBox(Collider col)
     {
-        // only handle the case of a player walking into it.
-        if (!other.IsPlayer())
+        if (!col.IsPlayer())
         {
             return;
         }
@@ -42,10 +45,23 @@ public class Monster : MonoBehaviour
         _playerHealth.TakeDamage(damage);
     }
 
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     // only handle the case of a player walking into it.
+    //     if (!other.IsPlayer())
+    //     {
+    //         return;
+    //     }
+    //
+    //     _animator.SetBool(Attacking, true);
+    //     _playerHealth.TakeDamage(damage);
+    // }
+
     public void Kill()
     {
         _collider.enabled = false;
         _rigidbody.isKinematic = true;
+        _areaTrigger.GetComponent<Collider>().enabled = false;
         _animator.SetTrigger(Die);
         // StartCoroutine(KillRoutine());
     }
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,20 +4,29 @@ public class PlayerAnimator : MonoBehaviour
 {
     private Animator _animator;
     private Player _player;
+    private Health _health;
 
     private static readonly int Attack1 = Animator.StringToHash("Attack1");
+    private static readonly int Die = Animator.StringToHash("Die");
 
 
     void Awake()
     {
         _animator = GetComponent<Animator>();
         _player = GetComponentInParent<Player>();
+        _health = _player.GetComponent<Health>();
     }
 
     private void Start()
     {
         _player.OnAttack += PlayAttackAnimation;
         _player.OnStopAttack += StopAttackAnimation;
+        _health.OnDeath += PlayDeathAnimation;
+    }
+
+    private void PlayDeathAnimation(Health health)
+    {
+        _animator.SetTrigger(Die);
     }
 
     private void PlayAttackAnimation()
@@ -25,7 +34,7 @@ public class PlayerAnimator : MonoBehaviour
         _animator.SetBool(Attack1, true);
     }
 
-    
+
     private void StopAttackAnimation()
     {
         _animator.SetBool(Attack1, false);

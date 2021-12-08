516476400db3f55fd9d71872392b45c2166be738
Cian Hatton <cianhatton@gmail.com>
2021-12-08 11:02:50 +0000

    almost fix jump

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -29,12 +29,8 @@ public class MoveLeft : MonoBehaviour
             return;
         }
 
-        Vector3 currentPosition = transform.position;
 
         // move the current position to the left every frame.
         transform.Translate(Vector3.left * amountToMoveToTheLeft * Time.deltaTime);
-        // transform.position =
-        // new Vector3(currentPosition.x - amountToMoveToTheLeft * Time.deltaTime, currentPosition.y,
-        // currentPosition.z);
     }
 }
